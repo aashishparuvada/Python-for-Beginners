611302890cfab104bd2cbce2b4974734626a2da9
Aashish Vardhan <79566248+aashishvardhan@users.noreply.github.com>
2021-07-24 13:04:01 +0530

    Add files via upload

---
+++ Beginners/Unpacking.py
@@ -0,0 +1,15 @@
+coordinates = (1, 2, 3)
+
+#x = coordinates[0]
+#y = coordinates[1]
+#z = coordinates[2]
+
+x, y, z = coordinates  # This is same as the piece of code in the lines 3,4,5
+
+print(x)
+print(y)
+print(z)
+print(x, y, z)
+
+
+# Unpacking is not limited only to tuples it is applicable to Lists also.
